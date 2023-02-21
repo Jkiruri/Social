8dadc36743882ba8b0e68f46bdd85bd295b9440c
James Kiruri <jamesnjunge45@gmail.com>
2023-02-21 10:42:09 +0300

    add file

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "./home.scss"
+
+function Home() {
+  return (
+    <div className='home'>Home</div>
+  )
+}
+
+export default Home
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,33 @@
+
+import "./register.scss"
+
+const Register = () => {
+  return (
+    <div className="register">
+        <div className="card">
+            <div className="left">
+                <h1>Wenz Social</h1>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "</p>
+          
+            <span> Have an account?</span>
+            
+           
+              <button>Login</button>
+          
+            </div>
+            <div className="right">
+                <h1>Register</h1>
+                <form>
+                <input type="text"  placeholder="Name"/>
+                <input type="text"  placeholder="Username"/>
+                <input type="email"  placeholder="Email"/>
+                <input type="password"  placeholder="Password"/>
+                <button>Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
