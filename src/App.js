1c9f355d91606dcb44538a9fb3b43bb2e1143187
James Kiruri <jamesnjunge45@gmail.com>
2023-02-23 11:53:50 +0300

    add  file

--- src/App.js
+++ src/App.js
@@ -11,13 +11,14 @@ import Home from "./pages/home/Home"
 import Profile from "./pages/profile/Profile"
 import { Navigate } from "react-router-dom";
 import { Outlet  } from "react-router-dom";
+import "./style.scss";
 
 function App() {
   const currentUser = true;
 
   const Layout = () =>{
     return(
-      <div>
+      <div className= "theme-light">
         <NavBar/>
         <div style={{ display:"flex" }}>
           <LeftBar/>
--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,8 +1,150 @@
 import "./rightBar.scss"
+import Dog from "../../assets/14.jpg";
 
 const RightBar = () => {
   return (
-    <div className="rightBar">RightBar
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <span>John Doe</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <span>John Doe</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+      <div className="item">
+        <span>Latest Activities</span>
+        <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <p>
+              <span>John Doe</span>
+              Changed their cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <p>
+              <span>John Doe</span>
+              Changed their cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <p>
+              <span>John Doe</span>
+              Changed their cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <p>
+              <span>John Doe</span>
+              Changed their cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <p>
+              <span>John Doe</span>
+              Changed their cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <p>
+              <span>John Doe</span>
+              Changed their cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+      </div>
+      <div className="item">
+        <span>Online Friends</span>
+        <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+              
+              
+            </div>
+           
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+              
+              
+            </div>
+           
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+              
+              
+            </div>
+           
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+              
+              
+            </div>
+           
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={Dog} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+              
+              
+            </div>
+           
+          </div>
+      </div>
+      </div>
     </div>
   )
 }
